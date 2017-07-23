796dd20e4a88561ce9cbdca14c510467a84c6458
John Louis Del Rosario <john2x@gmail.com>
2017-07-23 15:19:09 +0800

    Caching improvements

--- dank-cache.el
+++ dank-cache.el
@@ -2,7 +2,7 @@
 
 (defcustom dank-cache-enabled t "")
 (defcustom dank-cache-directory "cache" "")
-(defcustom dank-cache-timeout 3600 "")
+(defcustom dank-cache-timeout 60 "")
 
 (defun dank-cache--create-cache-directory ()
   "Create the cache directory `dank-cache-directory'."
@@ -25,14 +25,15 @@
     (buffer-string)))
 
 (defun dank-cache-get (key)
-  "Fetch contents of KEY from cache and return it as evaluated list.
-When AS-RAW-STRING is non-nil, then don't eval as a list."
-  (when (and dank-cache-enabled (file-readable-p (dank-cache--full-key key)))
+  "Fetch contents of KEY from cache and return it as evaluated list."
+  (when (and dank-cache-enabled (dank-cache-key-exists key))
     (dank-cache--read-cache (dank-cache--full-key key))))
 
 (defun dank-cache-key-exists (key)
-  "Check if KEY exists in cache."
-  (file-readable-p (dank-cache--full-key key)))
+  "Check if KEY exists in cache and is not expired."
+  (when (file-readable-p (dank-cache--full-key key))
+    (if (dank-cache-key-expired-p (key))
+        nil t)))
 
 (defun dank-cache-set (key value)
   "Set KEY in cache with VALUE."
@@ -41,12 +42,28 @@ When AS-RAW-STRING is non-nil, then don't eval as a list."
       (insert value))))
 
 (defun dank-cache-delete (key)
-  "Delete KEY from cache.")
+  "Delete KEY from cache."
+  (when (dank-cache-key-exists key)
+    (delete-file (dank-cache--full-key key))))
 
 (defun dank-cache-delete-all ()
-  "Delete everything in cache.")
+  "Delete everything in cache."
+  (delete-directory dank-cache-directory t)
+  (dank-cache--create-cache-directory))
 
-(defun dank-cache-cleanup ()
+(defun dank-cache-delete-expired ()
   "Delete files from cache that exceed `dank-cache-timeout'.")
+
+(defun dank-cache-key-expired-p (key &optional delete)
+  "Return t if KEY exists and is expired.
+If DELETE is non-nil, delete the key if it is expired."
+  (when (file-readable-p (dank-cache--full-key key))
+    (let* ((modified (nth 4 (file-attributes (dank-cache--full-key key))))
+           (now (current-time))
+           (modified-secs (+ (* (car modified) 65536) (cadr modified)))
+           (now-secs (+ (* (car now) 65536) (cadr now))))
+      (when (> (- now-secs modified-secs) dank-cache-timeout)
+        (if delete (dank-cache-delete key))
+        t))))
 
 (provide 'dank-cache)
