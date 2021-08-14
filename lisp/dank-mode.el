942cd5e5b646d6683e379957d890d40cf755e7a5
John Louis Del Rosario <john2x@gmail.com>
2021-08-14 20:15:28 +1200

    Clean up dank-mode and add comments

--- lisp/dank-mode.el
+++ lisp/dank-mode.el
@@ -4,50 +4,36 @@
 (require 'dank-posts)
 
 (defvar dank-mode-initialized nil)
-(defvar posts )
-(defun dank--render-posts-list (posts)
-  "")
-;; create new buffer
-;; fetch subreddits
-;; push to history
-;; render
-;; open subreddit
-;; push to history
-;; render
-
-(defun dank--render-post (post)
-  "")
 
 ;;;###autoload
 (defun dank-mode ()
+  "Entrypoint function for dank-mode.  Initialize a dank-posts-mode buffer for the reddit frontpage."
   (interactive)
   (unless dank-mode-initialized
     (dank-mode-init))
   (dank-posts-init nil))
 
-(defun dank-mode-reload ()
+(defun dank-mode-reset ()
+  "Reset dank-mode.  Kill all buffers, clear the cache, and re-initialize auth."
   (interactive)
-  (dank-mode-reset)
+  (setq dank-mode-initialized nil)
+  ;; TODO: kill all dank-mode buffers
   (dank-mode))
 
 (defun dank-mode-init ()
-  "Initialize dank-mode buffer."
+  "Initialize dank-mode cache and auth."
   (dank-mode--init-cache)
   (dank-mode--init-auth)
   (setq dank-mode-initialized t))
 
-
-(defun dank-mode-reset ()
-  (setq dank-mode-initialized nil))
-
 (defun dank-mode--init-cache ()
-  "Initialize dank-mode's cache (i.e. clear it)"
-  (message "clearing cache...")
+  "Initialize dank-mode's cache (i.e. clear it)."
+  (message "Clearing dank-cache...")
   (dank-cache-delete-all))
 
 (defun dank-mode--init-auth ()
   "Initialize dank-mode auth."
-  (message "init auth...")
+  (message "Initialize dank-auth...")
   (when dank-auth-file
     (unless (dank-auth-configured-p)
       (dank-auth-load-auth-vars-from-file dank-auth-file))))
