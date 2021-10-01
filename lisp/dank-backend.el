a0c819d2ab4c1b7f670ace247a5da8b84d48aceb
John Louis Del Rosario <john2x@gmail.com>
2021-10-02 10:42:40 +1300

    Cleanup dank-backend-request

--- lisp/dank-backend.el
+++ lisp/dank-backend.el
@@ -34,24 +34,34 @@
         (car (cdr request-args))
       (dank-backend--find-property (cdr request-args) property))))
 
-(defun dank-backend-request (method path &optional url-params)
-  "Perform a synchronous Reddit request with METHOD PATH and URL-PARAMS."
+(defun dank-backend-request (method path &optional url-params json-data)
+  "Perform a synchronous Reddit request with METHOD PATH and URL-PARAMS.
+JSON-DATA will be sent as the request body."
   (let* ((json-object-type 'plist)
          (encoded-url-params (dank-url-encode-alist url-params))
          (full-url (concat dank-backend-host path))
-         (full-url (if encoded-url-params (concat full-url "?" encoded-url-params) full-url)))
-      (let* ((token (dank-auth-token))
-             (authorization (concat "Bearer " token))
-             (url-user-agent dank-auth-user-agent)
-             (url-request-extra-headers `(("Authorization" . ,authorization)))
-             (response-buf (url-retrieve-synchronously full-url t t 60)))
-        (with-current-buffer response-buf
-          (if (= (dank-url-response-status-code) 200)
-              (progn
-                (json-parse-string (dank-url-response-uncompress) :object-type 'plist :null-object nil))
-            (progn
-              (signal 'dank-backend-request-error
-                      `(,url-params ,full-url ,(dank-url-response-uncompress)))))))))
+         (full-url (if encoded-url-params (concat full-url "?" encoded-url-params) full-url))
+         (token (dank-auth-token))
+         (authorization (concat "Bearer " token))
+         (url-user-agent dank-auth-user-agent)
+         (url-request-data (when json-data (json-encode-plist json-data)))
+         (url-request-method method)
+         (url-request-extra-headers `(("Authorization" . ,authorization)))
+         (response-buf (url-retrieve-synchronously full-url t t 60)))
+    ;; cleanup
+    (setq url-request-extra-headers nil
+          url-request-data nil
+          url-request-method nil)
+    (with-current-buffer response-buf
+      (let* ((response-status-code (dank-url-response-status-code))
+             (response-content-type (dank-url-response-header "content-type"))
+             (response-content (dank-url-response-uncompress))
+             (response-json (if (string-match-p "^application/json" response-content-type)
+                                (json-parse-string response-content :object-type 'plist :null-object nil))))
+        (if (= response-status-code 200)
+            (or response-json response-content)
+          (signal 'dank-backend-request-error
+                  `(,full-url ,method ,response-status-code ,response-content)))))))
 
 (defun dank-backend-post-listing (subreddit sorting &rest request-params)
   "Fetch authenticated user's SUBREDDIT posts sorted by SORTING.
@@ -74,7 +84,6 @@ If both :after and :before are provided, :after takes precedence and :before is 
            (params (if count (cons `(count . ,count) params) params))
            (params (if after (cons `(after . ,after) (assq-delete-all 'before params)) params))
            (resp (dank-backend-request "GET" url params)))
-      (message "%s" (plist-get resp :data))
       (plist-get (plist-get resp :data) :children))))
 
 (defun dank-backend-my-subreddits-listing (&rest request-params)
